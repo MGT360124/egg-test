3e34a31f21f98fa5ca6292ffb7fd2574e65ceb32
maoguotao <maoguotao@agree.com.cn>
2018-12-17 18:20:59 +0800

    增加配置

--- config/config.default.js
+++ config/config.default.js
@@ -6,8 +6,13 @@ module.exports = appInfo => {
     // use for cookie sign key, should change to your own and keep security
     config.keys = appInfo.name + '_1544862304952_8142';
 
-    // add your config here
+    // add your config here 中间件的配置
     config.middleware = [];
+    // 增加验证
+    exports.jwt = {
+        secret: "123456"
+    };
+    // 增加数据库连接
     config.sequelize = {
         dialect: 'mysql',
         host: '127.0.0.1',
--- config/plugin.js
+++ config/plugin.js
@@ -5,4 +5,30 @@
 exports.sequelize = {
     enable: true,
     package: 'egg-sequelize',
-  };
+};
+
+// Egg's JWT(JSON Web Token Authentication Plugin)
+exports.jwt = {
+    enable: true,
+    package: "egg-jwt"
+};
+
+exports.validate = {
+    enable: true,
+    package: "egg-validate"
+};
+
+exports.cors = {
+    enable: true,
+    package: "egg-cors"
+};
+
+exports.io = {
+    enable: true,
+    package: "egg-socket.io"
+};
+
+// exports.swaggerdoc = {
+//     enable: true,
+//     package: 'egg-swagger-builder',
+// };
