45984cf419438352f27f9b61ce3b3ce872bc22e2
maoguotao <maoguotao@agree.com.cn>
2019-03-17 09:35:35 +0800

    新增

--- app/model/user.js
+++ app/model/user.js
@@ -1,15 +1,24 @@
 'use strict';
 
 module.exports = app => {
-  const { STRING, INTEGER, DATE } = app.Sequelize;
+    const {
+        STRING,
+        INTEGER,
+        DATE
+    } = app.Sequelize;
 
-  const User = app.model.define('user', {
-    id: { type: INTEGER, primaryKey: true, autoIncrement: true },
-    name: STRING(30),
-    age: INTEGER,
-    created_at: DATE,
-    updated_at: DATE,
-  });
+    const User = app.model.define('user', {
+        id: {
+            type: INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: STRING(30),
+        age: INTEGER,
+        identityCard: INTEGER,
+        created_at: DATE,
+        updated_at: DATE,
+    });
 
-  return User;
+    return User;
 };
